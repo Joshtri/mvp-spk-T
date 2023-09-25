dabc7249fe891eb918f91f0f36a2d30581cefda0
Joshtri <stuffofyos1516@gmail.com>
2023-09-25 10:45:44 +0700

    adf

--- controllers/data1.js
+++ controllers/data1.js
@@ -1,9 +1,23 @@
 exports.data1_Page = (req,res)=>{
-    res.render('data_1');
+    res.render('data_1',
+    {
+        // notif condition.
+        notifSuksesTambah : false
+    });
+}
+
+exports.addData1_Page = (req,res)=>{
+    res.render('add_data_1',{
+        
+    });
 }
 
 
+//API TAMBAH DATA 
 
-exports.addData1_Page = (req,res)=>{
-    res.render('add_data_1');
+exports.post_Data1 = (req,res)=>{
+    
+    res.render('data_1',{
+        notifSuksesTambah : true
+    });
 }
--- router/data1.js
+++ router/data1.js
@@ -5,6 +5,6 @@ const data1Controller = require('../controllers/data1');
 
 
 router.get('/data_1', data1Controller.data1_Page);
-router.get('/add_data_1', data1Controller.data1_Page);
+router.get('/add_data_1', data1Controller.addData1_Page);
 
 module.exports = router;
